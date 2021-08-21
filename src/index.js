d4c44f309465722860d7027ed08686958181f00e
Sem-Chehovich <semchehovich@gmail.com>
2021-08-21 18:37:11 +0300

    init: complit task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+ module.exports = function check(str, bracketsConfig) {
+  let strConf = bracketsConfig.map(item => item.join(''))
+  while (strConf.length) {
+    let iterator = 0;
+    strConf.forEach(item => {
+      if (str.indexOf(item) !== -1) {
+        str = str.replace(item, '');
+      } else {
+        iterator += 1;
+      }
+    });
+
+    if (iterator === strConf.length) {
+      strConf.length = 0;
+    }
+  }
+
+  return !str.length
 }
